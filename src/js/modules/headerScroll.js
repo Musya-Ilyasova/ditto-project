dcc0e4171dce604190febcf2d0b10e619cd8dfd1
Musya <musya.ilyasova@getorca.app>
2023-05-22 14:20:19 +0300

    fixed

--- src/js/modules/headerScroll.js
+++ src/js/modules/headerScroll.js
@@ -2,15 +2,21 @@ function headerSticky() {
   const header = document.querySelector('.header');
   let position = window.scrollY;
   document.addEventListener('scroll', () => {
-    if(window.scrollY == 0) {
-      header.classList.add('show');
-      header.classList.remove('scroll');
+    if(window.scrollY <= 0) {
+      setTimeout(() => {
+        header.classList.add('show');
+        header.classList.remove('scroll');
+      }, '100');
     } else if(position != 0 && position > window.scrollY) {
-      header.classList.add('show');
-      header.classList.add('scroll');
+      setTimeout(() => {
+        header.classList.add('show');
+        header.classList.add('scroll');
+      }, '100');
     } else {
-      header.classList.remove('show');
-      header.classList.remove('scroll');
+      setTimeout(() => {
+        header.classList.remove('show');
+        header.classList.remove('scroll');
+      }, '100');
     };
     position = window.scrollY;
   })
