75635fa39b7b5dd9daa28c52f4a7a35b4d5b1141
Musya <musya.ilyasova@getorca.app>
2023-05-27 01:52:52 +0300

    header menu redesign

--- src/js/modules/headerScroll.js
+++ src/js/modules/headerScroll.js
@@ -1,5 +1,7 @@
 function headerSticky() {
   const header = document.querySelector('.header');
+  const links = document.querySelectorAll('.menu__link_submenu');
+  const overlay = document.querySelector('.overlay');
   let position = window.scrollY;
   document.addEventListener('scroll', () => {
     if(window.scrollY <= 0) {
@@ -13,6 +15,8 @@ function headerSticky() {
         header.classList.add('scroll');
       }, '100');
     } else {
+      links.forEach(item => item.classList.remove('active'));
+      overlay.style.display = "";
       setTimeout(() => {
         header.classList.remove('show');
         header.classList.remove('scroll');
--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -14,10 +14,31 @@ const closeMenu = (selectorClose, menu) => {
   })
 }
 
+const openSubMenu = (links, link, overlay) => {
+  links.forEach(item => item.classList.remove('active'));
+  link.classList.add('active');
+  overlay.style.display = "block";
+}
+
+const closeSubMenu = (links, overlay) => {
+  links.forEach(item => item.classList.remove('active'));
+  overlay.style.display = "";
+}
+
 const addMenu = () => {
-  const menu = document.querySelector('.header-menu__wrapper');
+  const menu = document.querySelector('.menu__wrapper');
+  const header = document.querySelector('.header');
+  const links = document.querySelectorAll('.menu__link_submenu');
+  let overlay = document.createElement('div');
+  overlay.classList.add('overlay');
+  document.body.prepend(overlay);
   openMenu('.header__burger', menu);
-  closeMenu('.header-menu__close', menu)
+  closeMenu('.menu__close', menu);
+  links.forEach(link => {
+    link.addEventListener('mouseover', () => openSubMenu(links, link, overlay));
+    link.addEventListener('click', () => openSubMenu(links, link, overlay));
+  });
+  overlay.addEventListener('click', () => closeSubMenu(links, overlay));
 }
 
 export default addMenu;
