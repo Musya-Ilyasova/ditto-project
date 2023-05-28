f2273a7d5a0b741b1dc657b3ca8f78ebb6b398db
Musya <musya.ilyasova@getorca.app>
2023-05-29 00:49:39 +0300

    add first screen

--- src/js/modules/firstScreenParralax.js
+++ src/js/modules/firstScreenParralax.js
@@ -4,22 +4,12 @@ const firstScreenParralaxImg = () => {
   imgY = img.offsetTop+(img.offsetHeight/2);
 
   let img1 = img.querySelector('picture:first-child'),
-      img2 = img.querySelector('picture:nth-child(2)'),
-      img3 = img.querySelector('picture:nth-child(3)'),
-      img4 = img.querySelector('picture:nth-child(4)'),
-      img5 = img.querySelector('picture:nth-child(5)'),
-      img6 = img.querySelector('picture:nth-child(6)');
+      img2 = img.querySelector('picture:nth-child(2)');
   window.addEventListener('mousemove', function(e) {
     let x = (e.clientX - imgX) / 100;
     let y = (e.clientY - imgY) / 100;
-    img3.style.transform = `translate3d(${x * 1}px, ${y * 2}px, 0 )`;
-    img4.style.transform = `translate3d(${x * 2}px, ${y * 2}px, 0 )`;
     img1.style.transform = `translate3d(${x * 4}px, ${y * 4}px, 0 )`;
     img2.style.transform = `translate3d(${x * 2}px, ${y * 2}px, 0 )`;
-    img5.style.transform = `translate3d(${x * 3}px, ${y * 3}px, 0 )`;
-    img6.style.transform = `translate3d(${x * 3}px, ${y * 4}px, 0 )`;
-    // document.querySelector('.subscription-first-image-desktop__back').style.transform = `translate3d(${x * 2}px, ${y * 2}px, 0 )`;
-    // document.querySelector('.subscription-first-image-desktop__front').style.transform = `translate3d(${x * 1}px, ${y * 1}px, 0 )`;
   })
 }
 
